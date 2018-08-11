6cc1ad449d4d94af4cb461c1d277466bb10e35fc
Lucas Werner <lwerner27@gmail.com>
2018-08-10 22:37:40 -0500

    added conditional loop example

--- 10-ForLoops/main.go
+++ 10-ForLoops/main.go
@@ -8,15 +8,36 @@ func main() {
 	// Most Basic for loop example
 	// Prints nummbers 0 through 9
 	// Basically the exact same syntax as javascript
-	for i := 0; i < 10; i++ {
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// Example of Nested Loop
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println("This is the outer loop: ", i)
+	// 	for j := 0; j < 3; j++ {
+	// 		fmt.Println("This is the INNER loop: ", j)
+	// 	}
+	// }
+
+	// Example of Conditional Loop
+	i := false
+	j := 0
+
+	for i == false {
+		// Print the value of j
 		fmt.Println(i)
+		fmt.Println(j)
+
+		// If j is equal to 9 change i to true
+		if j == 9 {
+			i = true
+		}
+
+		// Increment j if not equal to 9
+		j++
 	}
 
-	// Nested Loops
-	for i := 0; i < 10; i++ {
-		fmt.Println("This is the outer loop: ", i)
-		for j := 0; j < 3; j++ {
-			fmt.Println("This is the INNER loop: ", j)
-		}
-	}
+	fmt.Println(i)
+	fmt.Println("The Loop has ended")
 }
