b812f1e7faf7ba5651a003f2bcd186bbb41d07fc
Lucas Werner <lwerner27@gmail.com>
2018-08-10 22:13:02 -0500

    added basic for loop example

---
+++ 10-ForLoops/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// Most Basic for loop example
+	// Prints nummbers 0 through 9
+	// Basically the exact same syntax as javascript
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+
+	// Nested Loops
+}
