c68e67e8742dd92e357b1812adcff4a3988dac7b
Lucas Werner <lwerner27@gmail.com>
2018-08-21 21:27:36 -0500

    added more detail to variadic functions

--- 16-VariadicFuncs/main.go
+++ 16-VariadicFuncs/main.go
@@ -5,7 +5,11 @@ import (
 )
 
 func main() {
+	data := []float64{2, 3, 4, 5, 6}
 	fmt.Println("The average is ", average(5, 4))
+
+	// This will pull the date out of a list and send it to the function
+	fmt.Println("The average is ", average(data...))
 }
 
 // This function has a variadic parameter
