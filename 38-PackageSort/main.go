c107c50f6387a2a07bd26733682f55c504fe2086
Lucas Werner <lwerner27@gmail.com>
2018-09-25 23:11:22 -0500

    added some package-sort examples

---
+++ 38-PackageSort/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type people []string
+
+func main() {
+	// Basic Ascending Sort
+	studyGroup := people{"Josh", "Lucas", "Abbie", "Zak"}
+	// Sort Strings will work because our people type
+	// satisfies the slice of Interface interface for sort
+	fmt.Println(studyGroup)
+
+	// Just another example of using the sort package
+	numbers := []int{1, 5, 7, 2, 9, 4, 6, 3, 8}
+	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
+	fmt.Println(numbers)
+
+}
