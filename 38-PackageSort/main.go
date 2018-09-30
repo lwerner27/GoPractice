bb18fea6080c6a101ae96ff68fdee9d5d06352f7
Lucas Werner <lwerner27@gmail.com>
2018-09-29 23:34:00 -0500

    added a little more to the exmaples

--- 38-PackageSort/main.go
+++ 38-PackageSort/main.go
@@ -12,8 +12,9 @@ func main() {
 	studyGroup := people{"Josh", "Lucas", "Abbie", "Zak"}
 	// Sort Strings will work because our people type
 	// satisfies the slice of Interface interface for sort
-	fmt.Println(studyGroup)
-
+	fmt.Println("before sort: ", studyGroup)
+	sort.Strings(studyGroup)
+	fmt.Println("after sort: ", studyGroup)
 	// Just another example of using the sort package
 	numbers := []int{1, 5, 7, 2, 9, 4, 6, 3, 8}
 	sort.Sort(sort.Reverse(sort.IntSlice(numbers)))
